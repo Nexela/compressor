861d477eac94971f06552926b97bb5e91b70c0d0
Nexela <nexela@gmail.com>
2017-02-16 00:38:45 -0500

    Migration fix take2

--- migrations/migration_0.2.2.lua
+++ migrations/migration_0.2.2.lua
@@ -1,7 +1,7 @@
 for _, force in pairs(game.forces) do
     for i=1, 7 do
-        if game.forces[force.name].technologies["compression-"..i].researched then
-            for _, effect in pairs(game.forces[force.name].technologies["compression-"..i].effects) do
+        if force.technologies["compression-"..i].researched then
+            for _, effect in pairs(force.technologies["compression-"..i].effects) do
                 if effect.type == "unlock-recipe" then
                     effect.recipe.enabled = true
                 end
